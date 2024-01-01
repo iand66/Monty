91aa4f90ea66958973790741640b6bd3e07051da
iand66 <idavies66@gmail.com>
2024-01-01 14:50:57 +0000

    Supplemental Documentation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from lib.apputils import config, logSetup
-from orm.schema import Genre, Artist
+from orm.schema import Genre
 from orm.dbutils import dbInit, dbFill
 from orm.dbfunctions import dbSelectAll
 
@@ -8,8 +8,7 @@ if __name__ == '__main__':
     appcfg = config('./ini/globals.ini')
     logger = logSetup(appcfg['LOGCFG']['logcfg'], appcfg['LOGCFG']['logloc'], eval(appcfg['LOGCFG']['logecho']))
     engine = create_engine(appcfg['DBTST']['dbType'] + appcfg['DBTST']['dbName'])
-    dbInit(engine)
-    dbFill(engine,'./sam/csv/import.csv',appcfg['DBTST']['dbName'],False)
-    x = dbSelectAll(engine, Artist, True)
-    print(x)
-    
+    #dbInit(engine)
+    #dbFill(engine,'./sam/csv/import.csv',appcfg['DBTST']['dbName'],False)
+    #x = dbSelectAll(engine, Genre, eval(appcfg['LOGCFG']['trace']))
+    #print(type(x), x)
